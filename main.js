25b030fbb9a09852b7591b731659280f7ff86036
Drew <77373359+AjaxSelectButtonGames@users.noreply.github.com>
2025-11-30 13:54:45 -0500

    Fix typo in state variable name for counter

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
+// main.js
 import UI from "./ui.js";
 
 function render() {
   UI.begin();
 
-const [count, setCopunt] = UI.state("counter",0);
+  const [count, setCount] = UI.state("counter", 0);
 
   UI.row(() => {
     UI.button("➕ Increment", () => setCount(count + 1));
     UI.button("➖ Decrement", () => setCount(count - 1));
   });
 
-UI.button("Show Count", () => alert("Count: " + count));
+  UI.button("Show Count", () => alert("Count: " + count));
 
-UI.end(document.getElementById("app"));
+  UI.end(document.getElementById("app"));
 
-requestAnimationFrame(render);
+  requestAnimationFrame(render);
 }
 
 render();
