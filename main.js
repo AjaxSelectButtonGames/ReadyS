3bf3e2cf057879d5b6f3c4a59a6ef15d75c0c808
Drew <77373359+AjaxSelectButtonGames@users.noreply.github.com>
2025-11-30 13:47:01 -0500

    Refactor render function to include counter state

--- main.js
+++ main.js
@@ -2,9 +2,19 @@ import UI from "./ui.js";
 
 function render() {
   UI.begin();
-  UI.button("Click me");
-  UI.button("Another button");
-  UI.end(document.getElementById("app"));
+
+const [count, setCopunt] = UI.state("counter",0);
+
+UI.row(() => {
+  UI.button("+ Add", () => setCount(count + 1));
+  UI.button("- Subtract", => setCount(count - 1));
+});
+
+UI.button("Show Count", () => alert("Count: " + count));
+
+UI.end(document.getElementById("app"));
+
+requestAnimationFrame(render);
 }
 
 render();
